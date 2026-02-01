06a5a243cef431fc5390a9a211f4ac500f0a29f6
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2026-02-01 19:11:33 +0530

    clean project files for fresh rebuild

--- auth-service/src/main/java/com/pm/authservice/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.pm.authservice.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message){
-        super(message);
-    }
-}
--- patient-service/src/main/java/com/pm/patientservice/exception/EmailAlreadyException.java
+++
@@ -1,7 +0,0 @@
-package com.pm.patientservice.exception;
-
-public class EmailAlreadyException extends RuntimeException{
-    public EmailAlreadyException(String message) {
-        super(message);
-    }
-}
